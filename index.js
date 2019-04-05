c35a58934b96860a9aa31396a3cfb07643420ead
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2019-04-05 12:18:59 +0200

    Initial code

---
+++ index.js
@@ -0,0 +1,154 @@
+var instance_skel = require('../../instance_skel');
+var tcp = require('../../tcp');
+var debug;
+var log;
+
+function instance(system, id, config) {
+		var self = this;
+
+		// super-constructor
+		instance_skel.apply(this, arguments);
+		self.actions(); // export actions
+		return self;
+}
+
+instance.prototype.init = function () {
+		var self = this;
+
+		debug = self.debug;
+		log = self.log;
+
+		self.status(self.STATUS_UNKNOWN);
+
+		if (self.config.host !== undefined) {
+			self.tcp = new tcp(self.config.host, 43680);
+
+			self.tcp.on('status_change', function (status, message) {
+				self.status(status, message);
+			});
+
+			self.tcp.on('error', function () {
+				// Ignore
+			});
+		}
+};
+
+instance.prototype.updateConfig = function (config) {
+		var self = this;
+		self.config = config;
+
+		if (self.tcp !== undefined) {
+			self.tcp.destroy();
+			delete self.tcp;
+		}
+
+		if (self.config.host !== undefined) {
+			self.tcp = new tcp(self.config.host, 43680);
+
+			self.tcp.on('status_change', function (status, message) {
+				self.status(status, message);
+			});
+
+			self.tcp.on('error', function (message) {
+				// ignore for now
+			});
+		}
+};
+
+// Return config fields for web config
+instance.prototype.config_fields = function () {
+		var self = this;
+		return [
+			{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'Information',
+				value: 'This module is for Barco HDX'
+			},
+			{
+				type: 'textinput',
+				id: 'host',
+				label: 'Target IP',
+				width: 6,
+				default: '192.168.0.100',
+				regex: self.REGEX_IP
+			}
+		]
+};
+
+// When module gets deleted
+instance.prototype.destroy = function () {
+	var self = this;
+
+		if (self.tcp !== undefined) {
+			self.tcp.destroy();
+		}
+		debug("destroy", self.id);
+};
+
+instance.prototype.actions = function (system) {
+	var self = this;
+
+	var actions = {
+		'lamp': {
+			label: 'Lamp control',
+			options: [{
+				type: 'dropdown',
+				label: 'on/off',
+				id: 'lamp',
+				default: 'lamp_on',
+				choices: [{ label: 'lamp on', id: 'lamp_on' }, { label: 'lamp off', id: 'lamp_off' }]
+			}]
+		},
+		'shutter': {
+			label: 'Shutter',
+			options: [{
+				type: 'dropdown',
+				label: 'on/off',
+				id: 'Shutter',
+				default: 'shutter_close',
+				choices: [{ label: 'shutter close', id: 'shutter_close' }, { label: 'shutter open', id: 'shutter_open' }]
+			}]
+		}
+	};
+		self.setActions(actions);
+};
+
+
+instance.prototype.action = function (action) {
+	var self = this;
+	var id = action.action;
+	var opt = action.options;
+	var cmd;
+
+	switch (id) {
+
+		case 'lamp':
+			if (opt.lamp == 'lamp_on') {
+				cmd = '\xfe\x00\x00\x03\x02\x76\x1a\x01\x96\xff';
+			} else if (opt.lamp == 'lamp_off') {
+				cmd = '\xfe\x00\x00\x03\x02\x76\x1a\x00\x95\xff';
+			}
+			break
+
+		case 'shutter':
+			if (opt.lamp == 'shutter_open') {
+				cmd = '\xfe\x00\x22\x42\x00\x64\xff';
+			} else if (opt.lamp == 'shutter_close') {
+				cmd = '\xfe\x00\x23\x42\x00\x65\xff';
+			}
+			break
+
+	}
+
+	if (cmd !== undefined) {
+		if (self.tcp !== undefined) {
+			debug('sending ', cmd, "to", self.tcp.host);
+			self.tcp.send(cmd);
+		}
+	}
+};
+
+instance_skel.extendedBy(instance);
+exports = module.exports = instance;
